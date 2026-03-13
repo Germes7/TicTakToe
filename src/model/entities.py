bca3e39018964fd75929a2d84d042180c0d0a55f
Derek <ushakov_gd@mail.ru>
2026-03-13 22:14:11 +0300

    feat: доделал функционал Поля

--- src/model/entities.py
+++ src/model/entities.py
@@ -22,12 +22,12 @@ class Cell:  # Клас Клетка
 
     def set_marker(self, marker: int) -> None:  # Установить маркер (x, 0)
         if marker != MARKER_ZERO and marker != MARKER_CROSS:
-            raise ValueError(f"Не найден такой маркер {marker}")
+            raise ValueError(f"Не найден такой маркер: {marker}")
 
         self.__marker = marker
 
     def reset(self) -> None:  # Сбросить клетку
-        self.__marker = MARKER_EMPTY
+        self.__marker = MARKER_EMPTY  # Устанавливаем пустой маркер
 
 
 class Field:  # Клас Поле
@@ -41,6 +41,20 @@ class Field:  # Клас Поле
         self.__columns = columns  # Столбцы поля
 
         self.__cells = []
+
+    def try_make_move(self, x: int, y: int, marker):  # Попытка сделать ход (в поле)
+
+        if x < 0 or x >= self.__rows: raise ValueError()  # Проверили на координаты
+        if y < 0 or y >= self.__columns: raise ValueError()  # Проверили на координаты
+
+        if marker != MARKER_CROSS and marker != MARKER_ZERO: raise ValueError()  # Проверили на маркер
+
+        cell = self.__cells[x][y]  # Взяли клетку
+        if not cell.is_empty(): return False  # Если клетка не пустая, не можем сделать на нее ход
+
+        cell.set_marker(marker)  # Клетка пустая, ставим маркер
+
+        return True
 
     def create(self):  # Создаем поле
 
@@ -57,5 +71,3 @@ class Field:  # Клас Поле
             for j in range(0, self.__columns):
                 self.__cells[i][j].reset()
 
-    def make_move(self):  # Делаем ход (в поле)
-        pass
