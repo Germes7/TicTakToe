b94a8c5af16090b75d37d79f50be15632026f063
Derek <ushakov_gd@mail.ru>
2026-03-14 00:31:01 +0300

    feat: добавил скелет игры

--- src/controller.py
+++ src/controller.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 from model.entities import Field
+from model.constants import *
 
 class Referee:
 
@@ -22,3 +23,23 @@ class Referee:
 
     def __check_win_by_diagonal(self) -> bool:  # Проверка по диагоналям
         pass
+
+
+class Game:
+
+    def __init__(self):
+
+        self.__current_player = MARKER_EMPTY  # Игра знает о текущем игроке, кто сейчас ходит (по умолчанию Пустой маркер)
+
+        self.__field = Field(3, 3)  # Игра создала поле
+        self.referee = Referee(self.__field)  # Игра передала Судье знание о поле
+
+    def set_up(self) -> None:  # Включиться (поле создайся)
+        pass
+
+    def make_move(self, x, y,) -> None:  # Попытка сделать ход
+        pass
+
+    def finish(self) -> None:  # Конец игре
+        pass
+
